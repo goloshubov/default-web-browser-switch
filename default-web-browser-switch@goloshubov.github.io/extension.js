69db159f9b465f9183724ab4efa12574f47dd500
Dmitry Goloshubov <goloshubov@gmail.com>
2023-11-08 00:22:40 +0300

    gnome 45 support

--- default-web-browser-switch@goloshubov.github.io/extension.js
+++ default-web-browser-switch@goloshubov.github.io/extension.js
@@ -1,62 +1,52 @@
-'use strict';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import St from 'gi://St';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as ExtensionUtils from 'resource:///org/gnome/shell/misc/extensionUtils.js';
 
-const St = imports.gi.St;
-const Gio = imports.gi.Gio;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const GLib = imports.gi.GLib;
 const ByteArray = imports.byteArray;
-const Mainloop = imports.mainloop;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
 const FIREFOX_ICON = "firefox16.png";
 const CHROME_ICON = "chrome16.png";
 
-let button, icon, gicon_firefox, gicon_chrome;
 
+export default class DefaultBrowserExtension extends Extension {
+    enable() {
+      const gicon_firefox = Gio.icon_new_for_string(this.path + "/icons/" + FIREFOX_ICON);
+      const gicon_chrome = Gio.icon_new_for_string(this.path + "/icons/" + CHROME_ICON);
 
-function init(metadata) {
-}
+      this._button = new PanelMenu.Button(0.0);
+      this._icon = new St.Icon({ style_class: 'system-status-icon' });
 
+      let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings check default-web-browser firefox.desktop");
 
-function toggleDefaultBrowser() {
-  let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings check default-web-browser firefox.desktop");
-  if (ByteArray.toString(out).includes('yes')) {
-    icon.set_gicon(gicon_chrome)
-    //let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser google-chrome.desktop");
-    let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser com.google.Chrome.desktop");
-  } else {
-    icon.set_gicon(gicon_firefox)
-    let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser firefox.desktop");
-  }
-}
+      if (ByteArray.toString(out).includes('yes')) {
+        this._icon.set_gicon(gicon_firefox)
+      } else {
+        this._icon.set_gicon(gicon_chrome)
+      }
 
+      this._button.actor.add_actor(this._icon);
+      this._button.actor.connect('button-press-event', (item, event) => {
+          let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings check default-web-browser firefox.desktop");
+          if (ByteArray.toString(out).includes('yes')) {
+              this._icon.set_gicon(gicon_chrome)
+              //let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser google-chrome.desktop");
+              let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser com.google.Chrome.desktop");
+          } else {
+              this._icon.set_gicon(gicon_firefox)
+              let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings set default-web-browser firefox.desktop");
+          }
+      });
 
-function enable() {
-  button = new PanelMenu.Button(0.0);
-  icon = new St.Icon({ style_class: 'system-status-icon' });
-  gicon_firefox = Gio.icon_new_for_string(Me.path + "/icons/" + FIREFOX_ICON);
-  gicon_chrome = Gio.icon_new_for_string(Me.path + "/icons/" + CHROME_ICON);
+      Main.panel.addToStatusArea('default-web-browser', this._button);
+    }
 
-  let [ok, out, err, exit] = GLib.spawn_command_line_sync("xdg-settings check default-web-browser firefox.desktop");
-
-  if (ByteArray.toString(out).includes('yes')) {
-    icon.set_gicon(gicon_firefox)
-  } else {
-    icon.set_gicon(gicon_chrome)
-  }
-
-  button.actor.add_actor(icon);
-  button.actor.connect('button-press-event', toggleDefaultBrowser);
-  Main.panel.addToStatusArea('default-web-browser', button);
-}
-
-
-function disable() {
-  button.destroy();
-  button = null;
-  icon = null;
-  gicon_firefox = null;
-  gicon_chrome = null;
+    disable() {
+      this._button?.destroy();
+      this._button = null;
+      this._icon?.destroy();
+      this._icon = null;
+    }
 }
